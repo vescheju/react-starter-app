2c09b44eb6bb046b27b4b5d4139f84ee9fd05fda
Justin Vesche <vescheju@gmail.com>
2025-08-03 17:25:03 -0400

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+const config = {
+  trailingComma: 'es5',
+  semi: true,
+  printWidth: 120,
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  useTabs: false,
+  jsxSingleQuote: true,
+  bracketSpacing: false,
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  quoteProps: 'as-needed',
+  arrowParens: 'always',
+  singleAttributePerLine: false,
+};
+
+module.exports = config;
---
+++ eslint.config.cjs
@@ -0,0 +1,34 @@
+const {defineConfig} = require('eslint/config');
+const reactPlugin = require('eslint-plugin-react');
+const reactRefresh = require('eslint-plugin-react-refresh');
+const importPlugin = require('eslint-plugin-import');
+const eslintConfigPrettier = require('eslint-config-prettier/flat');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
+const reactHooks = require('eslint-plugin-react-hooks');
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const globals = require('globals');
+
+module.exports = defineConfig([
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  reactRefresh.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  jsxA11y.flatConfigs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname, // Root directory for tsconfig resolution
+      },
+    },
+  },
+]);
